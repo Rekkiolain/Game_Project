95ae38530524d9bd83515b9c94c3ee9f972dab4d
Isaiah <isaiah_manzo@outlook.com>
2024-11-09 11:36:14 +0100

    Level creation

---
+++ Level1/1.tsx
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="1" tilewidth="512" tileheight="288" tilecount="17" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Packs/Gothicvania Cemetery/Environment/background.png" width="384" height="224"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Packs/Gothicvania Cemetery/Environment/mountains.png" width="192" height="179"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Packs/Gothicvania Cemetery/Environment/graveyard.png" width="384" height="123"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Environments/caverns-files-web/layers/back-walls.png" width="512" height="288"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Environments/caverns-files-web/layers/background.png" width="192" height="288"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Environments/lava-background/PNG/background.png" width="432" height="240"/>
+ </tile>
+ <tile id="6">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Environments/lava-background/PNG/lava-background-preview.png" width="432" height="240"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Environments/lava-background/PNG/lava-tile.png" width="48" height="60"/>
+ </tile>
+ <tile id="8">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Environments/lava-background/PNG/middle-rocks.png" width="196" height="240"/>
+ </tile>
+ <tile id="9">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Environments/Mountain Dusk/version B/preview.png" width="320" height="240"/>
+ </tile>
+ <tile id="10">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Environments/night-town-background-files/layers/night-town-background-forest.png" width="64" height="96"/>
+ </tile>
+ <tile id="11">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Environments/night-town-background-files/layers/night-town-background-mountains.png" width="96" height="224"/>
+ </tile>
+ <tile id="12">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Environments/night-town-background-files/layers/night-town-background-mountains-lights.png" width="96" height="224"/>
+ </tile>
+ <tile id="13">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Environments/night-town-background-files/layers/night-town-background-sky.png" width="96" height="224"/>
+ </tile>
+ <tile id="14">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Environments/night-town-background-files/layers/night-town-background-far-buildings.png" width="320" height="80"/>
+ </tile>
+ <tile id="15">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Environments/night-town-background-files/layers/night-town-background-clouds.png" width="288" height="224"/>
+ </tile>
+ <tile id="16">
+  <image source="../../../Downloads/GameAssets/Legacy Collection/Legacy Collection/Assets/Environments/night-town-background-files/layers/night-town-background-town.png" width="512" height="99"/>
+ </tile>
+</tileset>
